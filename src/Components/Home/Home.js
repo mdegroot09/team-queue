427e9b2a39d0fcb21cf17824e86d332cacdc41a4
Mike De Groot <mdegroot09@gmail.com>
2019-11-22 06:29:38 -0700

    updated distance change to buttons

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -13,23 +13,27 @@ export default class Home extends Component {
         {name: 'Eric', zip: 84604}
       ]
     }
+    // var interval = ''
   }
 
-  handleInput(field, val){
+  handleInput = (field, val) => {
     this.setState({[field]: val})
   }
 
-  updateDist(val){
+  updateDist = (val) => {
     if(val >= 0){
       this.setState({dist: val})
     }
   }
 
-  // calcDist(){
-  //   var zip = this.state.zip
-  //   var wyatt = 84004
-  //   var dist = zipcodes.distance(wyatt, zip)
-  //   this.setState({dist: dist})
+  // updateDistFast = (val, dir) => {
+  //   if(dir === 'up'){
+  //     this.interval = setInterval(this.updateDist(this.state.dist))
+  //   }
+  // }
+
+  // cancelDistFast = () => {
+  //   window.clearInterval(this.interval)
   // }
 
   render(){
@@ -49,8 +53,18 @@ export default class Home extends Component {
       <div>
         <h1>Home</h1>
         <p>{this.state.dist} miles</p>
-        <button onClick={()=>this.updateDist(this.state.dist + 1)}>+</button>
-        <button onClick={()=>this.updateDist(this.state.dist - 1)}>-</button>
+        <button 
+          // onKeyDown={this.updateDistFast(this.state.dist, 'up')} 
+          // onKeyUp={this.cancelDistFast(this.state.dist, 'up')}
+          onClick={()=>this.updateDist(this.state.dist + 1)}>
+          +
+        </button>
+        <button 
+          // onKeyDown={this.updateDistFast(this.state.dist, 'down')} 
+          // onKeyUp={this.cancelDistFast(this.state.dist, 'down')}
+          onClick={()=>this.updateDist(this.state.dist - 1)}>
+          -
+        </button>
         <br/>
         <input onChange={(e)=>this.handleInput('zip', e.target.value)} type="number" placeholder='zip code'/>
         {/* <button onClick={()=>this.calcDist()}>Get Distance</button> */}
